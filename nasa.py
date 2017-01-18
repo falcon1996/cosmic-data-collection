8c141e567a7fb044d16c9c67d92a7555a0584e57
falcon1996 <dhruv.parashar31266@gmail.com>
2017-01-19 03:20:56 +0530

    Update nasa.py

--- nasa.py
+++ nasa.py
@@ -29,9 +29,9 @@ curiosity_sol = round((earth_day_time * earth_days) / mars_day_time)
 
 # FHAZ	              Front Hazard Avoidance Camera
 # RHAZ	              Rear Hazard Avoidance Camera
-# CHEMCAM	       Chemistry and Camera Complex
-# MAHLI	              Mars Hand Lens Imager
-# MARDI	              Mars Descent Imager
+# CHEMCAM	      Chemistry and Camera Complex
+# MAHLI	               Mars Hand Lens Imager
+# MARDI	               Mars Descent Imager
 # NAVCAM	       Navigation Camera
 # PANCAM	       Panoramic Camera
 # MINITES	       Miniature Thermal Emission Spectrometer (Mini-TES)
